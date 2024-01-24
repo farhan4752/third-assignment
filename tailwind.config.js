d7913892cb40098f81642ff5aa748f663c0a0b75
Farhan <farhansust02@gmail.com>
2024-01-24 22:56:48 +0600

    finished the work for small device

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'hero-pattern': "url('./Images/pic 1.png')",
+                'footer-texture': "url('/img/footer-texture.png')",
+            }
+        }
+    }
+}
